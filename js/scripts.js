3afc0440558ec615989816cd5dca7521367bc7f1
Warley Martins <warlinpb@Outlook.com>
2023-02-10 12:57:21 -0300

    publish

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+  // Ativar bootstrap na NAVBAR
+  const sideNav = document.body.querySelector("#sideNav");
+  if (sideNav) {
+    new bootstrap.ScrollSpy(document.body, {
+      target: "#sideNav",
+      offset: 74,
+    });
+  }
+
+  // Colapsar a navbar quando um link for clicado
+  const navbarToggler = document.body.querySelector(".navbar-toggler");
+  const responsiveNavItems = [].slice.call(
+    document.querySelectorAll("#navbarResponsive .nav-link")
+  );
+  responsiveNavItems.map(function (responsiveNavItem) {
+    responsiveNavItem.addEventListener("click", () => {
+      if (window.getComputedStyle(navbarToggler).display !== "none") {
+        navbarToggler.click();
+      }
+    });
+  });
+});
+
+// Dark Mode
+
+$(document).ready(function () {
+  $("#toggleDarkMode").click(function () {
+    if ($("body").hasClass("dark-mode")) {
+      $("body").removeClass("dark-mode");
+      $(this).text("Ativar Modo Claro");
+    } else {
+      $("body").addClass("dark-mode");
+      $(this).text("Ativar Modo Escuro");
+    }
+  });
+});
