f24199d54d12823eb58a15d0f2a6cc4a49085709
Warley Martins <warlinpb@Outlook.com>
2023-02-10 13:48:03 -0300

    att button DarkMode

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,40 @@
-window.addEventListener("DOMContentLoaded", (event) => {
-  // Ativar bootstrap na NAVBAR
-  const sideNav = document.body.querySelector("#sideNav");
-  if (sideNav) {
-    new bootstrap.ScrollSpy(document.body, {
-      target: "#sideNav",
-      offset: 74,
-    });
-  }
+window.addEventListener('DOMContentLoaded', event => {
 
-  // Colapsar a navbar quando um link for clicado
-  const navbarToggler = document.body.querySelector(".navbar-toggler");
-  const responsiveNavItems = [].slice.call(
-    document.querySelectorAll("#navbarResponsive .nav-link")
-  );
-  responsiveNavItems.map(function (responsiveNavItem) {
-    responsiveNavItem.addEventListener("click", () => {
-      if (window.getComputedStyle(navbarToggler).display !== "none") {
-        navbarToggler.click();
+    // Ativar bootstrap na NAVBAR
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            offset: 74,
+        });
+    };
+
+    // Colapsar a navbar quando um link for clicado
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+// Dark mode
+
+$(document).ready(function () {
+    $("#toggleDarkMode").click(function () {
+      if ($("body").hasClass("dark-mode")) {
+        $("body").removeClass("dark-mode");
+        $(this).html("<i class='fa fa-sun'></i>");
+      } else {
+        $("body").addClass("dark-mode");
+        $(this).html("<i class='fa fa-moon'></i>");
       }
     });
   });
-});
 
-// Dark Mode
-
-$(document).ready(function () {
-  $("#toggleDarkMode").click(function () {
-    if ($("body").hasClass("dark-mode")) {
-      $("body").removeClass("dark-mode");
-      $(this).text("Ativar Modo Claro");
-    } else {
-      $("body").addClass("dark-mode");
-      $(this).text("Ativar Modo Escuro");
-    }
-  });
-});
